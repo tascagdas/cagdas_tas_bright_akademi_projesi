78943a894d1f75bd44b16936bc4577d3d54bd01b
Çağdaş Taş <143749465+tascagdas@users.noreply.github.com>
2023-12-21 15:46:57 +0300

    Cdn aktif hale getirildi navbar eklendi.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import Footer from './Components/Footer/Footer';
+import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   return (
     <>
-      
+      <Navbar/>
       
     <Footer/>
     </>
