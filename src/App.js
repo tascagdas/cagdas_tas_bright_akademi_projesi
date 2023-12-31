b3bf5789eb3a276c992c6c074b40015fdf347c58
Çağdaş Taş <143749465+tascagdas@users.noreply.github.com>
2024-01-01 02:21:43 +0300

    Route sayfa eklemeye devam

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 
-import { BrowserRouter, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Footer from './Components/Footer/Footer';
 import Hero from './Components/Hero/Hero';
 import Navbar from './Components/Navbar/Navbar';
+import HomePage from './Pages/Home/Home';
+import AboutPage from './Pages/About/About.jsx';
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Navbar />
         <Routes>
-
+          <Route path='/' element={HomePage} />
+          <Route path='/about' element={AboutPage} />
         </Routes>
+        <Footer />
       </BrowserRouter>
-      <Navbar />
+
       <Hero />
-      <Footer />
+
     </>
   );
 }
