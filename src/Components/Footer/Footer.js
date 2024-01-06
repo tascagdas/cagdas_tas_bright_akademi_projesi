ef9edf3776f9dce44b25109988c3cc508b7bd662
Çağdaş Taş <143749465+tascagdas@users.noreply.github.com>
2024-01-06 14:55:05 +0300

    navbar footer edit

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,119 +1,77 @@
 import React from 'react'
+import logo from "../../images/logo.png";
 
 const Footer = () => {
     return (
         <>
-            <footer class="text-gray-600 body-font">
-                <div class="container px-5 py-24 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
-                    <div class="w-64 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left">
-                        <a class="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-green-500 rounded-full" viewBox="0 0 24 24">
-                                <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-                            </svg>
-                            <span class="ml-3 text-xl">Tailblocks</span>
-                        </a>
-                        <p class="mt-2 text-sm text-gray-500">Air plant banjo lyft occupy retro adaptogen indego</p>
-                    </div>
-                    <div class="flex-grow flex flex-wrap md:pl-20 -mb-10 md:mt-0 mt-10 md:text-left text-center">
-                        <div class="lg:w-1/4 md:w-1/2 w-full px-4">
-                            <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
-                            <nav class="list-none mb-10">
-                                <li>
-                                    <a class="text-gray-600 hover:text-gray-800">First Link</a>
-                                </li>
-                                <li>
-                                    <a class="text-gray-600 hover:text-gray-800">Second Link</a>
-                                </li>
-                                <li>
-                                    <a class="text-gray-600 hover:text-gray-800">Third Link</a>
-                                </li>
-                                <li>
-                                    <a class="text-gray-600 hover:text-gray-800">Fourth Link</a>
-                                </li>
-                            </nav>
+            <footer className="py-6 dark:bg-gray-800 dark:text-gray-50">
+                <div className="container px-6 mx-auto space-y-6 divide-y dark:divide-gray-400 md:space-y-12 divide-opacity-50">
+                    <div className="grid grid-cols-12">
+                        <div className="pb-6 col-span-full md:pb-0 md:col-span-6">
+                            <a rel="noopener noreferrer" href="#" className="flex justify-center space-x-3 md:justify-start">
+                                <img src={logo} alt="" />
+                            </a>
                         </div>
-                        <div class="lg:w-1/4 md:w-1/2 w-full px-4">
-                            <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
-                            <nav class="list-none mb-10">
+                        <div className="col-span-6 text-center md:text-left md:col-span-3">
+                            <p className="pb-1 text-lg font-medium underline">Eğitimler</p>
+                            <ul className='list-disc space-y-2'>
                                 <li>
-                                    <a class="text-gray-600 hover:text-gray-800">First Link</a>
+                                    <a rel="noopener noreferrer" href="#" className="hover:dark:text-violet-400">Fullstack Programlama Eğitimi</a>
                                 </li>
                                 <li>
-                                    <a class="text-gray-600 hover:text-gray-800">Second Link</a>
+                                    <a rel="noopener noreferrer" href="#" className="hover:dark:text-violet-400">Network, Sistem ve Siber Güvenlik Eğitimi</a>
                                 </li>
                                 <li>
-                                    <a class="text-gray-600 hover:text-gray-800">Third Link</a>
+                                    <a rel="noopener noreferrer" href="#" className="hover:dark:text-violet-400">Bulut Bilişim Uzmanlık Eğitimi</a>
                                 </li>
                                 <li>
-                                    <a class="text-gray-600 hover:text-gray-800">Fourth Link</a>
+                                    <a rel="noopener noreferrer" href="#" className="hover:dark:text-violet-400">Full Data Uzmanlık Eğitimi</a>
                                 </li>
-                            </nav>
+                            </ul>
                         </div>
-                        <div class="lg:w-1/4 md:w-1/2 w-full px-4">
-                            <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
-                            <nav class="list-none mb-10">
+                        <div className="col-span-6 text-center md:text-left md:col-span-3">
+                            <p className="pb-1 text-lg font-medium underline">Category</p>
+                            <ul className='list-disc space-y-2'>
                                 <li>
-                                    <a class="text-gray-600 hover:text-gray-800">First Link</a>
+                                    <a rel="noopener noreferrer" href="#" className="hover:dark:text-violet-400">Link</a>
                                 </li>
                                 <li>
-                                    <a class="text-gray-600 hover:text-gray-800">Second Link</a>
+                                    <a rel="noopener noreferrer" href="#" className="hover:dark:text-violet-400">Link</a>
                                 </li>
                                 <li>
-                                    <a class="text-gray-600 hover:text-gray-800">Third Link</a>
+                                    <a rel="noopener noreferrer" href="#" className="hover:dark:text-violet-400">Link</a>
                                 </li>
-                                <li>
-                                    <a class="text-gray-600 hover:text-gray-800">Fourth Link</a>
-                                </li>
-                            </nav>
-                        </div>
-                        <div class="lg:w-1/4 md:w-1/2 w-full px-4">
-                            <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
-                            <nav class="list-none mb-10">
-                                <li>
-                                    <a class="text-gray-600 hover:text-gray-800">First Link</a>
-                                </li>
-                                <li>
-                                    <a class="text-gray-600 hover:text-gray-800">Second Link</a>
-                                </li>
-                                <li>
-                                    <a class="text-gray-600 hover:text-gray-800">Third Link</a>
-                                </li>
-                                <li>
-                                    <a class="text-gray-600 hover:text-gray-800">Fourth Link</a>
-                                </li>
-                            </nav>
+                            </ul>
                         </div>
                     </div>
-                </div>
-                <div class="bg-gray-100">
-                    <div class="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
-                        <p class="text-gray-500 text-sm text-center sm:text-left">© 2020 Tailblocks —
-                            <a href="https://twitter.com/knyttneve" rel="noopener noreferrer" class="text-gray-600 ml-1" target="_blank">@knyttneve</a>
-                        </p>
-                        <span class="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
-                            <a class="text-gray-500">
-                                <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                                    <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+                    <div className="grid justify-center pt-6 lg:justify-between">
+                        <div className="flex flex-col self-center text-sm text-center md:block lg:col-start-1 md:space-x-6">
+                            <span>©2024 All rights reserved</span>
+                            <a rel="noopener noreferrer" href="#">
+                                <span>Privacy policy</span>
+                            </a>
+                            <a rel="noopener noreferrer" href="#">
+                                <span>Terms of service</span>
+                            </a>
+                        </div>
+                        <div className="flex justify-center pt-4 space-x-4 lg:pt-0 lg:col-end-13">
+                            <a rel="noopener noreferrer" href="#" title="Email" className="flex items-center justify-center w-10 h-10 rounded-full dark:bg-violet-400 dark:text-gray-900">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
+                                    <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path>
+                                    <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path>
                                 </svg>
                             </a>
-                            <a class="ml-3 text-gray-500">
-                                <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                                    <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                            <a rel="noopener noreferrer" href="#" title="Twitter" className="flex items-center justify-center w-10 h-10 rounded-full dark:bg-violet-400 dark:text-gray-900">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" fill="currentColor" className="w-5 h-5">
+                                    <path d="M 50.0625 10.4375 C 48.214844 11.257813 46.234375 11.808594 44.152344 12.058594 C 46.277344 10.785156 47.910156 8.769531 48.675781 6.371094 C 46.691406 7.546875 44.484375 8.402344 42.144531 8.863281 C 40.269531 6.863281 37.597656 5.617188 34.640625 5.617188 C 28.960938 5.617188 24.355469 10.21875 24.355469 15.898438 C 24.355469 16.703125 24.449219 17.488281 24.625 18.242188 C 16.078125 17.8125 8.503906 13.71875 3.429688 7.496094 C 2.542969 9.019531 2.039063 10.785156 2.039063 12.667969 C 2.039063 16.234375 3.851563 19.382813 6.613281 21.230469 C 4.925781 21.175781 3.339844 20.710938 1.953125 19.941406 C 1.953125 19.984375 1.953125 20.027344 1.953125 20.070313 C 1.953125 25.054688 5.5 29.207031 10.199219 30.15625 C 9.339844 30.390625 8.429688 30.515625 7.492188 30.515625 C 6.828125 30.515625 6.183594 30.453125 5.554688 30.328125 C 6.867188 34.410156 10.664063 37.390625 15.160156 37.472656 C 11.644531 40.230469 7.210938 41.871094 2.390625 41.871094 C 1.558594 41.871094 0.742188 41.824219 -0.0585938 41.726563 C 4.488281 44.648438 9.894531 46.347656 15.703125 46.347656 C 34.617188 46.347656 44.960938 30.679688 44.960938 17.09375 C 44.960938 16.648438 44.949219 16.199219 44.933594 15.761719 C 46.941406 14.3125 48.683594 12.5 50.0625 10.4375 Z"></path>
                                 </svg>
                             </a>
-                            <a class="ml-3 text-gray-500">
-                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                                    <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
-                                    <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+                            <a rel="noopener noreferrer" href="#" title="GitHub" className="flex items-center justify-center w-10 h-10 rounded-full dark:bg-violet-400 dark:text-gray-900">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" className="w-5 h-5">
+                                    <path d="M10.9,2.1c-4.6,0.5-8.3,4.2-8.8,8.7c-0.5,4.7,2.2,8.9,6.3,10.5C8.7,21.4,9,21.2,9,20.8v-1.6c0,0-0.4,0.1-0.9,0.1 c-1.4,0-2-1.2-2.1-1.9c-0.1-0.4-0.3-0.7-0.6-1C5.1,16.3,5,16.3,5,16.2C5,16,5.3,16,5.4,16c0.6,0,1.1,0.7,1.3,1c0.5,0.8,1.1,1,1.4,1 c0.4,0,0.7-0.1,0.9-0.2c0.1-0.7,0.4-1.4,1-1.8c-2.3-0.5-4-1.8-4-4c0-1.1,0.5-2.2,1.2-3C7.1,8.8,7,8.3,7,7.6C7,7.2,7,6.6,7.3,6 c0,0,1.4,0,2.8,1.3C10.6,7.1,11.3,7,12,7s1.4,0.1,2,0.3C15.3,6,16.8,6,16.8,6C17,6.6,17,7.2,17,7.6c0,0.8-0.1,1.2-0.2,1.4 c0.7,0.8,1.2,1.8,1.2,3c0,2.2-1.7,3.5-4,4c0.6,0.5,1,1.4,1,2.3v2.6c0,0.3,0.3,0.6,0.7,0.5c3.7-1.5,6.3-5.1,6.3-9.3 C22,6.1,16.9,1.4,10.9,2.1z"></path>
                                 </svg>
                             </a>
-                            <a class="ml-3 text-gray-500">
-                                <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="0" class="w-5 h-5" viewBox="0 0 24 24">
-                                    <path stroke="none" d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"></path>
-                                    <circle cx="4" cy="4" r="2" stroke="none"></circle>
-                                </svg>
-                            </a>
-                        </span>
+                        </div>
                     </div>
                 </div>
             </footer>
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -4,17 +4,17 @@ import logo from '../../images/logo.png'
 const Navbar = () => {
     return (
         <>
-            <header class="text-gray-600 body-font">
+            <header class="text-white body-font bg-green-700">
                 <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
                     <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
                         <img src={logo} alt="logo" />
                     </a>
                     <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
-                        <a href='/' class="mr-5 hover:text-gray-900">Ana Sayfa</a>
-                        <a href='about' class="mr-5 hover:text-gray-900">Hakkımızda</a>
-                        <a href='contact' class="mr-5 hover:text-gray-900">İletişim</a>
+                        <a href='/' class="mr-5 hover:text-green-950">Ana Sayfa</a>
+                        <a href='about' class="mr-5 hover:text-green-950">Hakkında</a>
+                        <a href='contact' class="mr-5 hover:text-green-950">İletişim</a>
                     </nav>
-                    <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Sizi Arayalım!
+                    <button class="inline-flex items-center bg-yellow-400 border-0 py-1 px-3 focus:outline-none hover:bg-yellow-500 rounded text-base mt-4 md:mt-0">Başvur
                         <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
                             <path d="M5 12h14M12 5l7 7-7 7"></path>
                         </svg>
