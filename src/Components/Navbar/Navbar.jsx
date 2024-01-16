73ab1d57df10f120631347384983871a9ba3da89
Çağdaş Taş <143749465+tascagdas@users.noreply.github.com>
2024-01-16 22:43:30 +0300

    Azbucuk responsiv geri geldi :D

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -43,9 +43,6 @@ const Navbar = () => {
                         </ul>
                     </nav>
                     <button className=" d-none d-sm-block inline-flex items-center bg-yellow-400 border-0 py-1 px-3 focus:outline-none hover:bg-yellow-500 rounded text-base mt-0 md:mt-0">Başvur!
-                        {/* <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
-                            <path d="M5 12h14M12 5l7 7-7 7"></path>
-                        </svg> */}
                     </button>
                 </div>
             </header>
